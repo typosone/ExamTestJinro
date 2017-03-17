76d91859fd1b65a2490f280bc682c83a39394c42
Nakasone Tsuyoshi <tsuyoshi.nakasone@gmail.com>
2017-03-17 11:00:26 +0900

    各アクティビティを起動するためのダミーアクティビティ実装 各担当者ごとのアクティビティ生成

--- app/src/main/java/jp/ac/it_college/std/nakasone/examtestjinro/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/nakasone/examtestjinro/MainActivity.java
@@ -1,7 +1,9 @@
 package jp.ac.it_college.std.nakasone.examtestjinro;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.run_title_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TitleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.run_game_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
